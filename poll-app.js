7e648137d0a49b41e59589f724cb6489c9239698
Jean809 <jean_carlosrosario709@outlook.com>
2022-09-17 09:23:18 -0400

    completed Poll-app

---
+++ poll-app.js
@@ -0,0 +1,42 @@
+"use strict";
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+};
+
+const dataPoints = {
+  //This is for testing the method, consider it useless unless you want to test something
+  data1: [5, 2, 3],
+  data2: [1, 5, 3, 9, 6, 1],
+};
+
+console.log(poll);
+
+const registeredAnswer = (poll.registerNewAnswer = function () {
+  const optionAnswer = Number(
+    prompt(
+      `${this.question}\n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}\n(write option number)`
+    )
+  );
+
+  if (optionAnswer <= this.answers.length) {
+    this.answers[optionAnswer]++;
+  } else {
+    alert(`${optionAnswer} is not within the options. Please try again.`);
+  }
+});
+
+registeredAnswer.displayResults = function (type = poll.answers) {
+  if (Array.isArray(type)) {
+    console.log([...type]);
+  } else if (typeof type === "string") {
+    console.log(`Poll results are ${type}`);
+  }
+};
+
+document.querySelector(".poll").addEventListener("click", function () {
+  poll.registerNewAnswer();
+  registeredAnswer.displayResults(`${poll.answers}`);
+});
